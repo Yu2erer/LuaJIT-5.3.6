3a5f1a5f8c89a837d47bfe2482f5b99675acd22f
Yu2erer <yu2erer@gmail.com>
2021-02-04 16:30:17 +0800

    add error tips

--- YGC.c
+++ YGC.c
@@ -5,6 +5,8 @@
 
 #include "YGC.h"
 
+#include <string.h>
+
 #include "lfunc.h"
 #include "ltable.h"
 #include "lstring.h"
@@ -17,15 +19,16 @@
 
 static void Y_linkrevert (global_State *g, GCObject *o);
 static void Y_closeupvalue (lua_State *L, UpVal *u);
-static void Y_reallymarkobject (global_State *g, GCObject *o, int b);
-static void Y_traverseproto (global_State *g, Proto *f, int b);
-static void Y_traverseLclosure (global_State *g, LClosure *cl, int b);
-static void Y_traversetable (global_State *g, Table *h, int b);
+static void Y_reallymarkobject (lua_State *L, GCObject *o, int b);
+static void Y_traverseproto (lua_State *L, Proto *f, int b);
+static void Y_traverseLclosure (lua_State *L, LClosure *cl, int b);
+static void Y_traversetable (lua_State *L, Table *h, int b);
+static int  Y_isweaktable (lua_State *L, const struct Table *h);
 
 #define Y_valis(x, b) (iscollectable(x) && (b ? !Y_isnogc(gcvalue(x)) : Y_isnogc(gcvalue(x))))
-#define Y_markvalue(g, o, b) { if (Y_valis(o, b)) Y_reallymarkobject(g, gcvalue(o), b); }
-#define Y_markobject(g, t, b) { Y_reallymarkobject(g, obj2gco(t), b); }
-#define Y_markobjectN(g, t, b) { if (t) Y_markobject(g, t, b); }
+#define Y_markvalue(L, o, b) { if (Y_valis(o, b)) Y_reallymarkobject(L, gcvalue(o), b); }
+#define Y_markobject(L, t, b) { Y_reallymarkobject(L, obj2gco(t), b); }
+#define Y_markobjectN(L, t, b) { if (t) Y_markobject(L, t, b); }
 #define Y_maskcolors (~(bitmask(BLACKBIT) | WHITEBITS))
 #define Y_makeblack(x) \
     (x->marked = cast_byte((x->marked & Y_maskcolors) | cast(lu_byte, bitmask(BLACKBIT))))
@@ -61,7 +64,8 @@ static void Y_closeupvalue (lua_State *L, UpVal *u) {
   }
 }
 
-static void Y_reallymarkobject (global_State *g, GCObject *o, int b) {
+static void Y_reallymarkobject (lua_State *L, GCObject *o, int b) {
+  global_State *g = G(L);
   switch (o->tt) {
     case LUA_TSHRSTR: {
       lu_mem mem = sizelstring(gco2ts(o)->shrlen);
@@ -84,12 +88,16 @@ static void Y_reallymarkobject (global_State *g, GCObject *o, int b) {
       break;
     }
     case LUA_TTABLE: {
+      if (Y_isweaktable(L, gco2t(o))) {
+        luaL_error(L, "Not support weak tables");
+        break;
+      }
       if (b) {
         Y_makenogc(o);
       } else {
         Y_resetobject(g, o);
       }
-      Y_traversetable(g, gco2t(o), b);
+      Y_traversetable(L, gco2t(o), b);
       break;
     }
     case LUA_TLCL: {
@@ -98,7 +106,7 @@ static void Y_reallymarkobject (global_State *g, GCObject *o, int b) {
       } else {
         Y_resetobject(g, o);
       }
-      Y_traverseLclosure(g, gco2lcl(o), b);
+      Y_traverseLclosure(L, gco2lcl(o), b);
       break;
     }
     case LUA_TPROTO: {
@@ -107,30 +115,42 @@ static void Y_reallymarkobject (global_State *g, GCObject *o, int b) {
       } else {
         Y_resetobject(g, o);
       }
-      Y_traverseproto(g, gco2p(o), b);
+      Y_traverseproto(L, gco2p(o), b);
+      break;
+    }
+    case LUA_TUSERDATA: {
+      luaL_error(L, "Not support userdata");
+      break;
+    }
+    case LUA_TTHREAD: {
+      luaL_error(L, "Not support thread");
+      break;
+    }
+    case LUA_TCCL: {
+      luaL_error(L, "Not support C function");
       break;
     }
     default: break;
   }
 }
 
-static void Y_traverseproto (global_State *g, Proto *f, int b) {
+static void Y_traverseproto (lua_State *L, Proto *f, int b) {
   if (f->cache && !Y_isnogc(f->cache)) {
     f->cache = NULL;
   }
   int i;
-  Y_markobjectN(g, f->source, b);
+  Y_markobjectN(L, f->source, b);
   for (i = 0; i < f->sizek; i ++) {
-    Y_markvalue(g, &f->k[i], b);
+    Y_markvalue(L, &f->k[i], b);
   }
   for (i = 0; i < f->sizeupvalues; i ++) {
-    Y_markobjectN(g, f->upvalues[i].name, b)
+    Y_markobjectN(L, f->upvalues[i].name, b)
   }
   for (i = 0; i < f->sizep; i ++) {
-    Y_markobjectN(g, f->p[i], b);
+    Y_markobjectN(L, f->p[i], b);
   }
   for (i = 0; i <f->sizelocvars; i ++) {
-    Y_markobjectN(g, f->locvars[i].varname, b);
+    Y_markobjectN(L, f->locvars[i].varname, b);
   }
   lu_mem mem = sizeof(Proto) + sizeof(Instruction) * f->sizecode +
                          sizeof(Proto *) * f->sizep +
@@ -138,13 +158,13 @@ static void Y_traverseproto (global_State *g, Proto *f, int b) {
                          sizeof(int) * f->sizelineinfo +
                          sizeof(LocVar) * f->sizelocvars +
                          sizeof(Upvaldesc) * f->sizeupvalues;
-  g->Y_GCmemnogc += (b ? mem : -mem);
+  G(L)->Y_GCmemnogc += (b ? mem : -mem);
 }
 
-static void Y_traverseLclosure (global_State *g, LClosure *cl, int b) {
+static void Y_traverseLclosure (lua_State *L, LClosure *cl, int b) {
   int i;
-  Y_markobjectN(g, cl->p, b);
-  Table *reg = hvalue(&g->l_registry);
+  Y_markobjectN(L, cl->p, b);
+  Table *reg = hvalue(&G(L)->l_registry);
   const TValue *gt = luaH_getint(reg, LUA_RIDX_GLOBALS);
   GCObject *gto = gcvalue(gt);
 
@@ -154,31 +174,43 @@ static void Y_traverseLclosure (global_State *g, LClosure *cl, int b) {
       GCObject *o = gcvalue(uv->v);
       if (o == gto || !b) continue;
       if (upisopen(uv)) {
-        Y_closeupvalue(g->mainthread, uv);
+        Y_closeupvalue(L, uv);
       }
-      Y_markvalue(g, uv->v, 1);
+      Y_markvalue(L, uv->v, 1);
     }
   }
   lu_mem mem = sizeLclosure(cl->nupvalues);
-  g->Y_GCmemnogc += (b ? mem : -mem);
+  G(L)->Y_GCmemnogc += (b ? mem : -mem);
 }
 
-static void Y_traversetable (global_State *g, Table *h, int b) {
-  Y_markobjectN(g, h->metatable, b);
+static void Y_traversetable (lua_State *L, Table *h, int b) {
+  Y_markobjectN(L, h->metatable, b);
   Node *n, *limit = gnode(h, cast(size_t, sizenode(h)));
   unsigned int i;
   for (i = 0; i < h->sizearray; i++) {
-    Y_markvalue(g, &h->array[i], b);
+    Y_markvalue(L, &h->array[i], b);
   }
   for (n = gnode(h, 0); n < limit; n++) {
     if (!ttisnil(gval(n))) {
-      Y_markvalue(g, gkey(n), b);
-      Y_markvalue(g, gval(n), b);
+      Y_markvalue(L, gkey(n), b);
+      Y_markvalue(L, gval(n), b);
     }
   }
   lu_mem mem = sizeof(Table) + sizeof(TValue) * h->sizearray +
                          sizeof(Node) * cast(size_t, allocsizenode(h));
-  g->Y_GCmemnogc += (b ? mem : -mem);
+  G(L)->Y_GCmemnogc += (b ? mem : -mem);
+}
+
+static int Y_isweaktable (lua_State *L, const struct Table *h) {
+  const char *weakkey, *weakvalue;
+  const TValue *mode = gfasttm(G(L), h->metatable, TM_MODE);
+  if (mode && ttisstring(mode) && 
+      ((weakkey = strchr(svalue(mode), 'k')),
+       (weakvalue = strchr(svalue(mode), 'v')),
+       (weakkey || weakvalue))) {
+    return 1;
+  }
+  return 0;
 }
 
 static const struct Table* Y_opttable (lua_State *L, int arg) {
@@ -197,7 +229,7 @@ static int Y_nogc (lua_State *L, int what, const struct Table *h) {
         luaL_argerror(L, 2, "Missing a table object");
         break;
       }
-      Y_markobject(g, h, Y_NOGCCCLOSE);
+      Y_markobject(L, h, Y_NOGCCCLOSE);
       break;
     }
     case Y_NOGCOPEN: {
@@ -205,7 +237,7 @@ static int Y_nogc (lua_State *L, int what, const struct Table *h) {
         luaL_argerror(L, 2, "Missing a table object");
         break;
       }
-      Y_markobject(g, h, Y_NOGCOPEN);
+      Y_markobject(L, h, Y_NOGCOPEN);
       break;
     }
     case Y_NOGCCOUNT: {
@@ -213,7 +245,7 @@ static int Y_nogc (lua_State *L, int what, const struct Table *h) {
       break;
     }
     case Y_NOGCLEN: {
-      GCObject *o = G(L)->Y_nogc;
+      GCObject *o = g->Y_nogc;
       while (o) {
         res ++;
         o = o->next;
